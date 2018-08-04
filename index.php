390108936b67d940b78c4cc08a6cc273c4e32fb9
benoit8391 <benoit8391@gmail.com>
2018-08-04 18:04:51 +0800

    git add --all
    init class_09

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Tshirts Kingdom</title>
+<link href="website.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="main">
+  <div id="header"> <img src="images/header.gif" width="834" height="60" />
+    <div id="topmenu"> <a href=".">回首頁</a>　 <a href="#">付款與配送方式</a>　 <a href="#">Ｔ恤質料與尺寸</a>　 <a href="#">我的購物車</a></div>
+    <form action="catalog.php" method="get" name="search" target="tsFrame" id="search">
+      <input name="key" type="text" class="type1" id="key" size="40" maxlength="40" />
+      <input name="button" type="submit" class="type1" id="button" value="搜尋" />
+    </form>
+    <div class="clearboth"></div>
+  </div>
+  <div id="containleft">
+    <div class="leftboard"> <img src="images/board_top1.gif" width="166" height="57" />
+      <ul>
+        <li>今日好康</li>
+        <li>產品類別</li>
+      </ul>
+      <div class="bottom"><img src="images/board_bottom.gif" width="166" height="26" /></div>
+    </div>
+    <div class="leftboard">
+      <div align="center"> <img src="images/board_top2.gif" width="166" height="63" /> <img src="imgshirt/001bs.jpg" width="109" height="91" />
+        <div class="bottom"><img src="images/board_bottom.gif" width="166" height="26" /></div>
+      </div>
+    </div>
+  </div>
+  <div id="containright"></div>
+  <div class="clearboth"></div>
+</div>
+</body>
+</html>
